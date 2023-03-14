7d524d9f255793f6f2ebd81d764b5b3b531453aa
Clayton Lawton <cla10law10@gmail.com>
2023-03-13 20:48:10 -0700

    Removed test and added lab default case

--- src/top.vhd
+++ src/top.vhd
@@ -11,7 +11,7 @@
 -- Copyright Digilent 2017
 ----------------------------------------------------------------------------------
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
 use IEEE.std_logic_unsigned.all;
 
 -- Uncomment the following library declaration if using
@@ -24,182 +24,172 @@ use IEEE.std_logic_unsigned.all;
 --use UNISIM.VComponents.all;
 
 entity top is
-    Port ( CLK_I : in  STD_LOGIC;
-           VGA_HS_O : out  STD_LOGIC;
-           VGA_VS_O : out  STD_LOGIC;
-           VGA_R : out  STD_LOGIC_VECTOR (3 downto 0);
-           VGA_B : out  STD_LOGIC_VECTOR (3 downto 0);
-           VGA_G : out  STD_LOGIC_VECTOR (3 downto 0));
+  port (
+    CLK_I    : in std_logic;
+    VGA_HS_O : out std_logic;
+    VGA_VS_O : out std_logic;
+    VGA_R    : out std_logic_vector (3 downto 0);
+    VGA_B    : out std_logic_vector (3 downto 0);
+    VGA_G    : out std_logic_vector (3 downto 0));
 end top;
 
 architecture Behavioral of top is
 
-component clk_wiz_0
-port
- (-- Clock in ports
-  CLK_IN1           : in     std_logic;
-  -- Clock out ports
-  CLK_OUT1          : out    std_logic
- );
-end component;
+  component clk_wiz_0
+    port (-- Clock in ports
+      CLK_IN1 : in std_logic;
+      -- Clock out ports
+      CLK_OUT1 : out std_logic
+    );
+  end component;
 
---Sync Generation constants
+  --Sync Generation constants
 
-----***640x480@60Hz***--  Requires 25 MHz clock
---constant FRAME_WIDTH : natural := 640;
---constant FRAME_HEIGHT : natural := 480;
+  ----***640x480@60Hz***--  Requires 25 MHz clock
+  --constant FRAME_WIDTH : natural := 640;
+  --constant FRAME_HEIGHT : natural := 480;
 
---constant H_FP : natural := 16; --H front porch width (pixels)
---constant H_PW : natural := 96; --H sync pulse width (pixels)
---constant H_MAX : natural := 800; --H total period (pixels)
+  --constant H_FP : natural := 16; --H front porch width (pixels)
+  --constant H_PW : natural := 96; --H sync pulse width (pixels)
+  --constant H_MAX : natural := 800; --H total period (pixels)
 
---constant V_FP : natural := 10; --V front porch width (lines)
---constant V_PW : natural := 2; --V sync pulse width (lines)
---constant V_MAX : natural := 525; --V total period (lines)
+  --constant V_FP : natural := 10; --V front porch width (lines)
+  --constant V_PW : natural := 2; --V sync pulse width (lines)
+  --constant V_MAX : natural := 525; --V total period (lines)
 
---constant H_POL : std_logic := '0';
---constant V_POL : std_logic := '0';
+  --constant H_POL : std_logic := '0';
+  --constant V_POL : std_logic := '0';
 
-----***800x600@60Hz***--  Requires 40 MHz clock
---constant FRAME_WIDTH : natural := 800;
---constant FRAME_HEIGHT : natural := 600;
---
---constant H_FP : natural := 40; --H front porch width (pixels)
---constant H_PW : natural := 128; --H sync pulse width (pixels)
---constant H_MAX : natural := 1056; --H total period (pixels)
---
---constant V_FP : natural := 1; --V front porch width (lines)
---constant V_PW : natural := 4; --V sync pulse width (lines)
---constant V_MAX : natural := 628; --V total period (lines)
---
---constant H_POL : std_logic := '1';
---constant V_POL : std_logic := '1';
+  ----***800x600@60Hz***--  Requires 40 MHz clock
+  --constant FRAME_WIDTH : natural := 800;
+  --constant FRAME_HEIGHT : natural := 600;
+  --
+  --constant H_FP : natural := 40; --H front porch width (pixels)
+  --constant H_PW : natural := 128; --H sync pulse width (pixels)
+  --constant H_MAX : natural := 1056; --H total period (pixels)
+  --
+  --constant V_FP : natural := 1; --V front porch width (lines)
+  --constant V_PW : natural := 4; --V sync pulse width (lines)
+  --constant V_MAX : natural := 628; --V total period (lines)
+  --
+  --constant H_POL : std_logic := '1';
+  --constant V_POL : std_logic := '1';
+  ----***1280x720@60Hz***-- Requires 74.25 MHz clock
+  --constant FRAME_WIDTH : natural := 1280;
+  --constant FRAME_HEIGHT : natural := 720;
+  --
+  --constant H_FP : natural := 110; --H front porch width (pixels)
+  --constant H_PW : natural := 40; --H sync pulse width (pixels)
+  --constant H_MAX : natural := 1650; --H total period (pixels)
+  --
+  --constant V_FP : natural := 5; --V front porch width (lines)
+  --constant V_PW : natural := 5; --V sync pulse width (lines)
+  --constant V_MAX : natural := 750; --V total period (lines)
+  --
+  --constant H_POL : std_logic := '1';
+  --constant V_POL : std_logic := '1';
 
+  ----***1280x1024@60Hz***-- Requires 108 MHz clock
+  --constant FRAME_WIDTH : natural := 1280;
+  --constant FRAME_HEIGHT : natural := 1024;
 
-----***1280x720@60Hz***-- Requires 74.25 MHz clock
---constant FRAME_WIDTH : natural := 1280;
---constant FRAME_HEIGHT : natural := 720;
---
---constant H_FP : natural := 110; --H front porch width (pixels)
---constant H_PW : natural := 40; --H sync pulse width (pixels)
---constant H_MAX : natural := 1650; --H total period (pixels)
---
---constant V_FP : natural := 5; --V front porch width (lines)
---constant V_PW : natural := 5; --V sync pulse width (lines)
---constant V_MAX : natural := 750; --V total period (lines)
---
---constant H_POL : std_logic := '1';
---constant V_POL : std_logic := '1';
+  --constant H_FP : natural := 48; --H front porch width (pixels)
+  --constant H_PW : natural := 112; --H sync pulse width (pixels)
+  --constant H_MAX : natural := 1688; --H total period (pixels)
 
-----***1280x1024@60Hz***-- Requires 108 MHz clock
---constant FRAME_WIDTH : natural := 1280;
---constant FRAME_HEIGHT : natural := 1024;
+  --constant V_FP : natural := 1; --V front porch width (lines)
+  --constant V_PW : natural := 3; --V sync pulse width (lines)
+  --constant V_MAX : natural := 1066; --V total period (lines)
 
---constant H_FP : natural := 48; --H front porch width (pixels)
---constant H_PW : natural := 112; --H sync pulse width (pixels)
---constant H_MAX : natural := 1688; --H total period (pixels)
+  --constant H_POL : std_logic := '1';
+  --constant V_POL : std_logic := '1';
 
---constant V_FP : natural := 1; --V front porch width (lines)
---constant V_PW : natural := 3; --V sync pulse width (lines)
---constant V_MAX : natural := 1066; --V total period (lines)
+  --***1920x1080@60Hz***-- Requires 148.5 MHz pxl_clk
+  constant FRAME_WIDTH  : natural := 1920;
+  constant FRAME_HEIGHT : natural := 1080;
 
---constant H_POL : std_logic := '1';
---constant V_POL : std_logic := '1';
+  constant H_FP  : natural := 88;   --H front porch width (pixels)
+  constant H_PW  : natural := 44;   --H sync pulse width (pixels)
+  constant H_MAX : natural := 2200; --H total period (pixels)
 
---***1920x1080@60Hz***-- Requires 148.5 MHz pxl_clk
-constant FRAME_WIDTH : natural := 1920;
-constant FRAME_HEIGHT : natural := 1080;
+  constant V_FP  : natural := 4;    --V front porch width (lines)
+  constant V_PW  : natural := 5;    --V sync pulse width (lines)
+  constant V_MAX : natural := 1125; --V total period (lines)
 
-constant H_FP : natural := 88; --H front porch width (pixels)
-constant H_PW : natural := 44; --H sync pulse width (pixels)
-constant H_MAX : natural := 2200; --H total period (pixels)
+  constant H_POL : std_logic := '1';
+  constant V_POL : std_logic := '1';
 
-constant V_FP : natural := 4; --V front porch width (lines)
-constant V_PW : natural := 5; --V sync pulse width (lines)
-constant V_MAX : natural := 1125; --V total period (lines)
+  --Moving Box constants
+  constant BOX_WIDTH   : natural := 8;
+  constant BOX_CLK_DIV : natural := 1000000; --MAX=(2^25 - 1)
 
-constant H_POL : std_logic := '1';
-constant V_POL : std_logic := '1';
+  constant BOX_X_MAX : natural := (512 - BOX_WIDTH);
+  constant BOX_Y_MAX : natural := (FRAME_HEIGHT - BOX_WIDTH);
 
---Moving Box constants
-constant BOX_WIDTH : natural := 8;
-constant BOX_CLK_DIV : natural := 1000000; --MAX=(2^25 - 1)
+  constant BOX_X_MIN : natural := 0;
+  constant BOX_Y_MIN : natural := 256;
 
-constant BOX_X_MAX : natural := (512 - BOX_WIDTH);
-constant BOX_Y_MAX : natural := (FRAME_HEIGHT - BOX_WIDTH);
+  constant BOX_X_INIT : std_logic_vector(11 downto 0) := x"000";
+  constant BOX_Y_INIT : std_logic_vector(11 downto 0) := x"190"; --400
 
-constant BOX_X_MIN : natural := 0;
-constant BOX_Y_MIN : natural := 256;
+  signal pxl_clk : std_logic;
+  signal active  : std_logic;
 
-constant BOX_X_INIT : std_logic_vector(11 downto 0) := x"000";
-constant BOX_Y_INIT : std_logic_vector(11 downto 0) := x"190"; --400
+  signal h_cntr_reg : std_logic_vector(11 downto 0) := (others => '0');
+  signal v_cntr_reg : std_logic_vector(11 downto 0) := (others => '0');
 
-signal pxl_clk : std_logic;
-signal active : std_logic;
+  signal h_sync_reg : std_logic := not(H_POL);
+  signal v_sync_reg : std_logic := not(V_POL);
 
-signal h_cntr_reg : std_logic_vector(11 downto 0) := (others =>'0');
-signal v_cntr_reg : std_logic_vector(11 downto 0) := (others =>'0');
+  signal h_sync_dly_reg : std_logic := not(H_POL);
+  signal v_sync_dly_reg : std_logic := not(V_POL);
 
-signal h_sync_reg : std_logic := not(H_POL);
-signal v_sync_reg : std_logic := not(V_POL);
+  signal vga_red_reg   : std_logic_vector(3 downto 0) := (others => '0');
+  signal vga_green_reg : std_logic_vector(3 downto 0) := (others => '0');
+  signal vga_blue_reg  : std_logic_vector(3 downto 0) := (others => '0');
 
-signal h_sync_dly_reg : std_logic := not(H_POL);
-signal v_sync_dly_reg : std_logic :=  not(V_POL);
+  signal vga_red   : std_logic_vector(3 downto 0);
+  signal vga_green : std_logic_vector(3 downto 0);
+  signal vga_blue  : std_logic_vector(3 downto 0);
 
-signal vga_red_reg : std_logic_vector(3 downto 0) := (others =>'0');
-signal vga_green_reg : std_logic_vector(3 downto 0) := (others =>'0');
-signal vga_blue_reg : std_logic_vector(3 downto 0) := (others =>'0');
+  signal box_x_reg    : std_logic_vector(11 downto 0) := BOX_X_INIT;
+  signal box_x_dir    : std_logic                     := '1';
+  signal box_y_reg    : std_logic_vector(11 downto 0) := BOX_Y_INIT;
+  signal box_y_dir    : std_logic                     := '1';
+  signal box_cntr_reg : std_logic_vector(24 downto 0) := (others => '0');
 
-signal vga_red : std_logic_vector(3 downto 0);
-signal vga_green : std_logic_vector(3 downto 0);
-signal vga_blue : std_logic_vector(3 downto 0);
-
-signal box_x_reg : std_logic_vector(11 downto 0) := BOX_X_INIT;
-signal box_x_dir : std_logic := '1';
-signal box_y_reg : std_logic_vector(11 downto 0) := BOX_Y_INIT;
-signal box_y_dir : std_logic := '1';
-signal box_cntr_reg : std_logic_vector(24 downto 0) := (others =>'0');
-
-signal update_box : std_logic;
-signal pixel_in_box : std_logic;
-
-
+  signal update_box   : std_logic;
+  signal pixel_in_box : std_logic;
 begin
-  
-   
-clk_div_inst : clk_wiz_0
+  clk_div_inst : clk_wiz_0
   port map
-   (-- Clock in ports
+  (-- Clock in ports
     CLK_IN1 => CLK_I,
     -- Clock out ports
     CLK_OUT1 => pxl_clk);
-
-  
   ----------------------------------------------------
   -------         TEST PATTERN LOGIC           -------
   ----------------------------------------------------
   vga_red <= h_cntr_reg(5 downto 2) when (active = '1' and ((h_cntr_reg < 512 and v_cntr_reg < 256) and h_cntr_reg(8) = '1')) else
-              (others=>'1')         when (active = '1' and ((h_cntr_reg < 512 and not(v_cntr_reg < 256)) and not(pixel_in_box = '1'))) else
-              (others=>'1')         when (active = '1' and ((not(h_cntr_reg < 512) and (v_cntr_reg(8) = '1' and h_cntr_reg(3) = '1')) or
-                                          (not(h_cntr_reg < 512) and (v_cntr_reg(8) = '0' and v_cntr_reg(3) = '1')))) else
-              (others=>'0');
-                
-  vga_blue <= h_cntr_reg(5 downto 2) when (active = '1' and ((h_cntr_reg < 512 and v_cntr_reg < 256) and  h_cntr_reg(6) = '1')) else
-              (others=>'1')          when (active = '1' and ((h_cntr_reg < 512 and not(v_cntr_reg < 256)) and not(pixel_in_box = '1'))) else 
-              (others=>'1')          when (active = '1' and ((not(h_cntr_reg < 512) and (v_cntr_reg(8) = '1' and h_cntr_reg(3) = '1')) or
-                                           (not(h_cntr_reg < 512) and (v_cntr_reg(8) = '0' and v_cntr_reg(3) = '1')))) else
-              (others=>'0');  
-              
+    (others => '1') when (active = '1' and ((h_cntr_reg < 512 and not(v_cntr_reg < 256)) and not(pixel_in_box = '1'))) else
+    (others => '1') when (active = '1' and ((not(h_cntr_reg < 512) and (v_cntr_reg(8) = '1' and h_cntr_reg(3) = '1')) or
+    (not(h_cntr_reg < 512) and (v_cntr_reg(8) = '0' and v_cntr_reg(3) = '1')))) else
+    (others => '0');
+
+  vga_blue <= h_cntr_reg(5 downto 2) when (active = '1' and ((h_cntr_reg < 512 and v_cntr_reg < 256) and h_cntr_reg(6) = '1')) else
+    (others => '1') when (active = '1' and ((h_cntr_reg < 512 and not(v_cntr_reg < 256)) and not(pixel_in_box = '1'))) else
+    (others => '1') when (active = '1' and ((not(h_cntr_reg < 512) and (v_cntr_reg(8) = '1' and h_cntr_reg(3) = '1')) or
+    (not(h_cntr_reg < 512) and (v_cntr_reg(8) = '0' and v_cntr_reg(3) = '1')))) else
+    (others => '0');
+
   vga_green <= h_cntr_reg(5 downto 2) when (active = '1' and ((h_cntr_reg < 512 and v_cntr_reg < 256) and h_cntr_reg(7) = '1')) else
-              (others=>'1')           when (active = '1' and ((h_cntr_reg < 512 and not(v_cntr_reg < 256)) and not(pixel_in_box = '1'))) else 
-              (others=>'1')           when (active = '1' and ((not(h_cntr_reg < 512) and (v_cntr_reg(8) = '1' and h_cntr_reg(3) = '1')) or
-                                            (not(h_cntr_reg < 512) and (v_cntr_reg(8) = '0' and v_cntr_reg(3) = '1')))) else
-              (others=>'0');
-              
- 
- ------------------------------------------------------
- -------         MOVING BOX LOGIC                ------
- ------------------------------------------------------
+    (others => '1') when (active = '1' and ((h_cntr_reg < 512 and not(v_cntr_reg < 256)) and not(pixel_in_box = '1'))) else
+    (others => '1') when (active = '1' and ((not(h_cntr_reg < 512) and (v_cntr_reg(8) = '1' and h_cntr_reg(3) = '1')) or
+    (not(h_cntr_reg < 512) and (v_cntr_reg(8) = '0' and v_cntr_reg(3) = '1')))) else
+    (others => '0');
+  ------------------------------------------------------
+  -------         MOVING BOX LOGIC                ------
+  ------------------------------------------------------
   process (pxl_clk)
   begin
     if (rising_edge(pxl_clk)) then
@@ -217,7 +207,7 @@ clk_div_inst : clk_wiz_0
       end if;
     end if;
   end process;
-      
+
   process (pxl_clk)
   begin
     if (rising_edge(pxl_clk)) then
@@ -231,52 +221,50 @@ clk_div_inst : clk_wiz_0
       end if;
     end if;
   end process;
-  
+
   process (pxl_clk)
   begin
     if (rising_edge(pxl_clk)) then
       if (box_cntr_reg = (BOX_CLK_DIV - 1)) then
-        box_cntr_reg <= (others=>'0');
+        box_cntr_reg <= (others => '0');
       else
-        box_cntr_reg <= box_cntr_reg + 1;     
+        box_cntr_reg <= box_cntr_reg + 1;
       end if;
     end if;
   end process;
-  
+
   update_box <= '1' when box_cntr_reg = (BOX_CLK_DIV - 1) else
-                '0';
-                
+    '0';
+
   pixel_in_box <= '1' when (((h_cntr_reg >= box_x_reg) and (h_cntr_reg < (box_x_reg + BOX_WIDTH))) and
-                            ((v_cntr_reg >= box_y_reg) and (v_cntr_reg < (box_y_reg + BOX_WIDTH)))) else
-                  '0';
-                
-  
- ------------------------------------------------------
- -------         SYNC GENERATION                 ------
- ------------------------------------------------------
- 
+    ((v_cntr_reg >= box_y_reg) and (v_cntr_reg < (box_y_reg + BOX_WIDTH)))) else
+    '0';
+  ------------------------------------------------------
+  -------         SYNC GENERATION                 ------
+  ------------------------------------------------------
+
   process (pxl_clk)
   begin
     if (rising_edge(pxl_clk)) then
       if (h_cntr_reg = (H_MAX - 1)) then
-        h_cntr_reg <= (others =>'0');
+        h_cntr_reg <= (others => '0');
       else
         h_cntr_reg <= h_cntr_reg + 1;
       end if;
     end if;
   end process;
-  
+
   process (pxl_clk)
   begin
     if (rising_edge(pxl_clk)) then
       if ((h_cntr_reg = (H_MAX - 1)) and (v_cntr_reg = (V_MAX - 1))) then
-        v_cntr_reg <= (others =>'0');
+        v_cntr_reg <= (others => '0');
       elsif (h_cntr_reg = (H_MAX - 1)) then
         v_cntr_reg <= v_cntr_reg + 1;
       end if;
     end if;
   end process;
-  
+
   process (pxl_clk)
   begin
     if (rising_edge(pxl_clk)) then
@@ -287,8 +275,6 @@ clk_div_inst : clk_wiz_0
       end if;
     end if;
   end process;
-  
-  
   process (pxl_clk)
   begin
     if (rising_edge(pxl_clk)) then
@@ -299,26 +285,24 @@ clk_div_inst : clk_wiz_0
       end if;
     end if;
   end process;
-  
-  
   active <= '1' when ((h_cntr_reg < FRAME_WIDTH) and (v_cntr_reg < FRAME_HEIGHT))else
-            '0';
+    '0';
 
   process (pxl_clk)
   begin
     if (rising_edge(pxl_clk)) then
       v_sync_dly_reg <= v_sync_reg;
       h_sync_dly_reg <= h_sync_reg;
-      vga_red_reg <= vga_red;
-      vga_green_reg <= vga_green;
-      vga_blue_reg <= vga_blue;
+      vga_red_reg    <= vga_red;
+      vga_green_reg  <= vga_green;
+      vga_blue_reg   <= vga_blue;
     end if;
   end process;
 
   VGA_HS_O <= h_sync_dly_reg;
   VGA_VS_O <= v_sync_dly_reg;
-  VGA_R <= vga_red_reg;
-  VGA_G <= vga_green_reg;
-  VGA_B <= vga_blue_reg;
+  VGA_R    <= vga_red_reg;
+  VGA_G    <= vga_green_reg;
+  VGA_B    <= vga_blue_reg;
 
 end Behavioral;
